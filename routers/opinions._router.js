cbfcb4b9c6dbe0a9abaaa041135f17cdb2993749
Nasser ALhseen <abdalnasralhseen2019@gmail.com>
2023-08-21 16:18:45 +0300

    post change

--- routers/opinions._router.js
+++ routers/opinions._router.js
@@ -4,5 +4,5 @@ const router = express.Router();
 
 router.post('/upload', control.upload);
 router.delete('/delete/:id', control.delete);
-router.get('/all', control.all);
+router.post('/all', control.all);
 export default router;
--- routers/servicesar.js
+++ routers/servicesar.js
@@ -4,5 +4,5 @@ const router = express.Router();
 
 router.post('/upload/:lang', control.upload);
 router.delete('/delete/:id', control.delete);
-router.get('/all/:lang', control.all);
+router.post('/all/:lang', control.all);
 export default router;
